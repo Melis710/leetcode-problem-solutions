d0acc0aeca319af3991057acc385f812bfc604a7
Melis Şahin <melisshnhkfl@gmail.com>
2026-07-20 23:53:51 +0300

    leetcode 167 two sum 2 problem solution

--- 167-two-sum-2.py
+++ 167-two-sum-2.py
@@ -1,11 +1,11 @@
-def twoSum(nums: list[int], target: int) -> list[int]:
+def twoSum(nums: list[int], target_sum: int) -> list[int]:
     i, j = 0, len(nums) - 1  # initialize pointers
     curr_sum = nums[i] + nums[j]  # current sum, given length >= 2
 
-    while i < j and curr_sum != target:
-        if curr_sum > target:  # too big, move left
+    while i < j and curr_sum != target_sum:
+        if curr_sum > target_sum:  # too big, move left
             j -= 1
-        if curr_sum < target:  # too small, move right
+        if curr_sum < target_sum:  # too small, move right
             i += 1 
         curr_sum = nums[i] + nums[j]  # update current sum
 
