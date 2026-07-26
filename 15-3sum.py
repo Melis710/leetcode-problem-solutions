cfb562624ffb1a7317cba42b331722042af48b6b
Melis Şahin <melisshnhkfl@gmail.com>
2026-07-26 06:49:14 +0300

    leetcode-15 3Sum problem optimized solution

--- 15-3sum.py
+++ 15-3sum.py
@@ -1,35 +1,32 @@
 def threeSum(nums: list[int]) -> list[list[int]]:
-    nums.sort()  # sort to handle duplicates and two sum problem
-    res = []  # list of triples to be returned
+    nums.sort()  # sort to handle duplicates
     n = len(nums)
-    i = 0
-    while i < n:
-        # fix the first number to reduce the problem to Two Sum
-        num1 = nums[i]  
-        target = -num1  # target sum
+    res = []  # triplets
+
+    for i in range(n):
+        if nums[i] > 0:  # early exit since array is sorted, impossible to have nums[j] + nums[k] < 0
+            break
+
+        if i > 0 and nums[i] == nums[i-1]:  # skip same targets
+            continue
+
+        ## TwoSum Problem
+        target_sum = -nums[i]
         j, k = i + 1, n - 1
-        ## Two Sum Problem: find all pairs with target sum
         while j < k:
             curr_sum = nums[j] + nums[k]
-            if curr_sum == target:
-                num2, num3 = nums[j], nums[k]
-                res.append([num1, num2, num3])
-                # move one step
+            if curr_sum == target_sum:
+                res.append([nums[i], nums[j], nums[k]])
                 j += 1
                 k -= 1
-                # skip same num2s to avoid duplicates
-                while j < k and nums[j] == nums[j-1]: 
+                while j < k and nums[j] == nums[j-1]:  # skip duplicate second numbers
                     j += 1
-                # skip same num3s to avoid duplicates
+                # while skipping j's only is enough, skipping k's too is for optimization here
                 while j < k and nums[k] == nums[k+1]:
                     k -= 1
-            elif curr_sum < target:  # move right
+            elif curr_sum > target_sum:
+                k -= 1
+            else:  # curr_sum < target_sum
                 j += 1
-            else:
-                k -= 1  # move left
-        i += 1
-        # skip same target sums (num1s)
-        while i < n and nums[i] == nums[i-1]:  
-            i += 1
-
+        
     return res
