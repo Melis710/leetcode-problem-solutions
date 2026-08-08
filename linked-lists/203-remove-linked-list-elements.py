78bd390744616b6de0fb132e85ec69dafe829592
Melis Şahin <melisshnhkfl@gmail.com>
2026-08-08 03:07:11 +0300

    leetcode-203 remove linked list elements solution

--- linked-lists/203-remove-linked-list-elements.py
+++ linked-lists/203-remove-linked-list-elements.py
@@ -1,10 +1,13 @@
 def removeElements(head, val):
+    # use dummy (sentinel) node since head can change (can be deleted)
     dummy = curr = ListNode(next=head)
 
+    # in order not to hold a prev pointer, look ahead by curr.next instead
     while curr.next:
-        if curr.next.val == val:
+        if curr.next.val == val:  # if equal, delete 
+            # do not advance curr pointer since new curr.next might be equal to val
             curr.next = curr.next.next
-        else:
+        else:  # if not equal, proceed
             curr = curr.next
 
-    return dummy.next
+    return dummy.next  # return actual head
