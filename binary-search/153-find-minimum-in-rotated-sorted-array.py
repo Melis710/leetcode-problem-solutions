2d1138339b311919d3839ff5bdb4b5b2e2427360
Melis Şahin <melisshnhkfl@gmail.com>
2026-08-11 01:24:34 +0300

    leetcode-153 solution update

--- binary-search/153-find-minimum-in-rotated-sorted-array.py
+++ binary-search/153-find-minimum-in-rotated-sorted-array.py
@@ -12,3 +12,18 @@ class Solution:
                 i = mid + 1
 
         return min_val
+
+class Solution2:
+    ## Binary Search using True-False matching pattern
+    def findMin(self, nums: List[int]) -> int:
+        i, j = 0, len(nums) - 1  # initialize pointers from both ends
+
+        while i <= j:
+            mid = (i + j)//2
+            if nums[mid] <= nums[-1]:  # feasibility function satisfied 
+                j = mid - 1  # search left for a lower value
+            else:
+                i = mid + 1  # search right half
+
+        return nums[i]
+
