b100f6ce76dc98db56726c9b11de674030bd56a0
Melis Şahin <melisshnhkfl@gmail.com>
2026-08-11 21:37:28 +0300

    leetcode-733 two solution

--- breadth-first-search/733-flood-fill.py
+++ breadth-first-search/733-flood-fill.py
@@ -1,6 +1,7 @@
 from collections import deque
 
 class Solution:
+    ## BFS solution
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         if image[sr][sc] == color:  # early exit
             return image
@@ -28,3 +29,34 @@ class Solution:
                 queue.append((up, sc))
             
         return image
+
+class Solution2:
+    ## DFS solution
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        if image[sr][sc] == color:  # early exit
+            return image
+
+        starting_color = image[sr][sc]  # save starting pixel's color before changing it
+        m, n = len(image), len(image[0])  # dimensions (# of rows, # of columns)
+
+        # define the Depth First Search function
+        def dfs(root):
+            sr, sc = root
+            ## Base Cases
+            if not (0 <= sr < m and 0 <= sc < n):  # invalid coordinates?
+                return
+            if image[sr][sc] != starting_color:  # not of same color?
+                return
+
+            ## Recursive Case
+            image[sr][sc] = color  # mark as visited (change the color)
+            # explore neighbors
+            dfs((sr - 1, sc))  # up
+            dfs((sr + 1, sc))  # down
+            dfs((sr, sc - 1))  # left
+            dfs((sr, sc + 1))  # right
+
+        # call recursive dfs function
+        dfs((sr, sc))
+
+        return image  # return resulting image
