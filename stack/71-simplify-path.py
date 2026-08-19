e4f82ef03a206dab5db23845847674d8644cb015
Melis Şahin <melisshnhkfl@gmail.com>
2026-08-19 03:28:05 +0300

    leetcode-71 simplify path solution

---
+++ stack/71-simplify-path.py
@@ -0,0 +1,17 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []  # stack for undo operations (..)
+
+        for d in path.split("/"):  # for each directory name 
+            if d == "" or d == ".":
+                continue
+            elif d == "..":  # go parent directory (backward - stack undo operation)
+                if stack:
+                    stack.pop()
+            else:  # add directory name (forward - push onto the directory stack)
+                stack.append(d)
+        
+        return "/" + "/".join(stack)
+
+            
+    
